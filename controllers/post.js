d599772aa89981e30b7dc18e553b7c4b9c8c0a7d
al-amin <alamin@priyo.com>
2022-04-17 00:05:35 +0600

    update post api done

--- controllers/post.js
+++ controllers/post.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js'
 export const getPost = async (req,res)=>{
     try {
@@ -17,4 +18,12 @@ export const createPost = async (req,res)=>{
     } catch (error) {
         res.status(409).json({message: error.message})
     }
+}
+
+export const updatePost = async (req,res)=>{
+    const { id: _id} = req.params;
+    const post = req.body;
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post found with this Id')
+    const updatePost = await PostMessage.findByIdAndUpdate(_id,{...post, _id},{new:true})
+    res.json(updatePost)
 }
--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,12 @@
 import express  from "express";
 
 //File import
-import { getPost, createPost } from '../controllers/post.js'
+import { getPost, createPost, updatePost } from '../controllers/post.js'
 
 const router = express.Router();
 
 router.get('/', getPost);
 router.post('/', createPost);
+router.patch('/:id', updatePost);
 
 export default router;
